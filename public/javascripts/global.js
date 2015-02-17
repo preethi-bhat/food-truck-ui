54c5d8dfa1d38595776fb0272f9e378e18db8f76
Preethi Bhat <prbhat@hotwire.com>
2015-02-16 23:03:17 -0800

    Adding winner selection toggle switch

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -15,120 +15,94 @@ $(document).ready(function() {
 // Functions =============================================================
 
 // Fill table with data
-function populateTable() {
+function populateTable(data) {
 
     // Empty content string
     var tableContent = '';
+    var ckb = $("#winnerselection").is(':checked');
 
-   //alert('hello');
-    // jQuery AJAX call for JSON
-    $.getJSON( 'http://localhost:3001/car/results', function( data ) {
     $('#carResults table tbody tr').remove()
      var row = 0;
         $.each(data.results, function(index, solution){
             $.each(solution.pgoods, function(index, pgood) {
-                var tableContent = ""
+                if (ckb) {
+                    if (pgood.isWinner) {
+                        var tableContent = ""
 
-                row++;
-                var isOpaque = 'Y';
-                if(pgood.isOpaque == false) {
-                    isOpaque = 'N';
+                        row++;
+                        var isOpaque = 'Y';
+                        if(pgood.isOpaque == false) {
+                            isOpaque = 'N';
+                        }
+                        tableContent += '<tr class="item-' + row%2 + '">';
+                        tableContent += '<td>' + pgood.carType + '</td>';
+                        tableContent += '<td>' + pgood.carVendor + '</td>';
+                        tableContent += '<td>' + pgood.price + '</td>';
+                        tableContent += '<td>' + pgood.depositType +'</td>';
+                        tableContent += '<td>' + isOpaque +'</td>'
+                        tableContent += '</tr>';
+                        $('#carResults table').last().append(tableContent);
+                    }
                 }
-                tableContent += '<tr class="item-' + row%2 + '">';
-                tableContent += '<td>' + pgood.carType + '</td>';
-                tableContent += '<td>' + pgood.carVendor + '</td>';
-                tableContent += '<td>' + pgood.price + '</td>';
-                tableContent += '<td>' + pgood.depositType +'</td>';
-                tableContent += '<td>' + isOpaque +'</td>'
-                tableContent += '</tr>';
-                $('#carResults table').last().append(tableContent);
+                else {
+                        var tableContent = ""
+
+                        row++;
+                        var isOpaque = 'Y';
+                        if(pgood.isOpaque == false) {
+                            isOpaque = 'N';
+                        }
+                        tableContent += '<tr class="item-' + row%2 + '">';
+                        tableContent += '<td>' + pgood.carType + '</td>';
+                        tableContent += '<td>' + pgood.carVendor + '</td>';
+                        tableContent += '<td>' + pgood.price + '</td>';
+                        tableContent += '<td>' + pgood.depositType +'</td>';
+                        tableContent += '<td>' + isOpaque +'</td>'
+                        tableContent += '</tr>';
+                        $('#carResults table').last().append(tableContent);
+
+                };
+                
             })
+            $('#carResults table').last().append('<tr class="divider"><td> </td></tr>');
         })
-    });
 };
 
 
 
 function doCarSearch(event) {
     event.preventDefault();
-
+    var ckb = $("#winnerselection").is(':checked');
 
     // If it is, compile all user info into one object
     var carSearchParams = {
         'destination': 'Houston,TX',
         'checkin': '3/1/2015',
-        'checkout': '3/2/2015'
+        'checkout': '3/2/2015',
+        'winnerselection' : ckb
     }
 
     var start = new Date().getTime();
-    $.ajax({
-        type: 'POST',
-        data: carSearchParams,
-        url: 'http://localhost:3001/car/search',
-        dataType: 'JSON'
-    });
-    var executionTime = new Date().getTime() - start;
 
-    populateTable();
-    $('#carResultsTime').text('Execution time (ms) = ' + executionTime);
+    $.ajax({
+        url : 'http://localhost:3001/car/results',
+        type: "POST",
+        data : carSearchParams,
+        success: function(data, textStatus, jqXHR)
+        {
+            //data - response from server
+            var executionTime = new Date().getTime() - start;
+            populateTable(data);
+            $('#carResultsTime').text('Execution time (ms) = ' + data.executionTime);
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+     
+        }
+    });
+    
 }
 
-// Add User
-function addUser(event) {
-    event.preventDefault();
-
-
-    // Super basic validation - increase errorCount variable if any fields are blank
-    var errorCount = 0;
-    $('#addUser input').each(function(index, val) {
-        if($(this).val() === '') { errorCount++; }
-    });
-
-    // Check and make sure errorCount's still at zero
-    if(errorCount === 0) {
-
-        // If it is, compile all user info into one object
-        var newUser = {
-            'username': $('#addUser fieldset input#inputUserName').val(),
-            'email': $('#addUser fieldset input#inputUserEmail').val(),
-            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
-            'age': $('#addUser fieldset input#inputUserAge').val(),
-            'location': $('#addUser fieldset input#inputUserLocation').val(),
-            'gender': $('#addUser fieldset input#inputUserGender').val()
-        }
-
-        // Use AJAX to post the object to our adduser service
-        $.ajax({
-            type: 'POST',
-            data: newUser,
-            url: '/users/adduser',
-            dataType: 'JSONP'
-        }).done(function( response ) {
-
-            // Check for successful (blank) response
-            if (response.msg === '') {
-
-                // Clear the form inputs
-                $('#addUser fieldset input').val('');
-
-                // Update the table
-                populateTable();
-
-            }
-            else {
-
-                // If something goes wrong, alert the error message that our service returned
-                alert('Error: ' + response.msg);
-
-            }
-        });
-    }
-    else {
-        // If errorCount is more than 0, error out
-        alert('Please fill in all fields');
-        return false;
-    }
-};
 
 
 
